71433faed5dfe50eb99c6615e2ee4c357099480a
lianer <lianer@live.cn>
2021-09-15 00:18:31 +0800

    update

--- 524.通过删除字母匹配到字典里最长单词.js
+++ 524.通过删除字母匹配到字典里最长单词.js
@@ -4,6 +4,8 @@
  * [524] 通过删除字母匹配到字典里最长单词
  */
 
+// ! 字典序：字典序是指从前到后比较两个字符串的大小的方法。首先比较第一个字符，如果不同则第一个字符较小的字符串更小，如果相同则继续比较第2个字符......如此继续，来比较整个字符串的大小。
+
 // @lc code=start
 /**
  * @param {string} s
@@ -27,10 +29,7 @@ var findLongestWord = function (s, dictionary) {
       }
     }
     if (wi === word.length) {
-      if (
-        word.length > maxStr.length ||
-        (word.length === maxStr.length && word < maxStr)
-      ) {
+      if (word.length > maxStr.length || (word.length === maxStr.length && word < maxStr)) {
         maxStr = word;
       }
     }
