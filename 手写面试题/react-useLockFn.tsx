64b4b8697a22a087813f733982e7c03d53917ac9
louyl <louyl@wedoctor.com>
2022-07-05 17:06:10 +0800

    update

--- 手写面试题/react-useLockFn.tsx
+++ 手写面试题/react-useLockFn.tsx
@@ -4,6 +4,7 @@ import { useCallback, useRef } from 'react';
 // 当 Promise 任务进入的时候，上锁
 // 当 Promise 任务完成的时候，解锁
 
+// useLockFn 可以实现逻辑层的 lock 功能，通过 useRef 储存 lock 状态，避免 lock 状态变更导致无用的组件渲染
 function useLockFn<P extends any[] = any[], V extends any = any>(fn: (...args: P) => Promise<V>) {
   const lockRef = useRef(false);
 
